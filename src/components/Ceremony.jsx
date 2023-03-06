b997a9331a6e3db2e6bd6e6466de149c66919228
Bryan <bryansanudo95@gmail.com>
2023-03-06 16:00:29 -0500

    padrinos arras

--- src/components/Ceremony.jsx
+++ src/components/Ceremony.jsx
@@ -7,6 +7,7 @@ import padrinosSacramento from '@/assets/fathers/padrinosSacramento.jpg'
 //import padrinosArras from '@/assets/fathers/padrinosArras.jpg'
 import padrinosAnillos from '@/assets/fathers/padrinosAnillos.jpg'
 import padrinosLazo from '@/assets/fathers/padrinosLazo.jpg'
+import padrinosArras from '@/assets/fathers/padrinosArras.jpg'
 
 
 
@@ -21,7 +22,7 @@ import {  GiDiamondRing } from "react-icons/gi";
 
 
 const ARRAY_IMG = [
-    padresDeivi,padresSorany,padrinosSacramento,padrinosAnillos,padrinosLazo
+    padresDeivi,padresSorany,padrinosSacramento,padrinosArras,padrinosAnillos,padrinosLazo
 ]
 
 const Ceremony = () => {
